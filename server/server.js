0b94a4a78f25838821d3ded4452570ec29f44c97
Darshit Sojitra <sojitradarshitpiyushbhai@gmail.com>
2025-11-10 16:01:17 +0530

    Update CORS methods in server.js
    Removed PATCH and OPTIONS methods from CORS configuration.

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ app.use(
             "http://localhost:5173",
             "http://localhost:5174",
         ],
-        methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+        methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true,
     })
 );
