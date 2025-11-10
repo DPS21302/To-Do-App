e719967d6568f3597ce182bda7cac1d413de884e
Darshit Sojitra <sojitradarshitpiyushbhai@gmail.com>
2025-11-10 16:19:06 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -26,6 +26,7 @@ app.use(
         allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"]
     })
 );
+app.options('*', cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
